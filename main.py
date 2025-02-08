0adf226ae9489beb17a64aae2d4d59516893df33
rydeblack <ryhad.boughanmi@gmail.com>
2025-02-08 15:30:15 +0100

    add Ship replace part and material method and add RacingShip class

--- main.py
+++ main.py
@@ -18,8 +18,35 @@ class Ship:
         
     def display_state(self):
         for part_name, part in self.__parts.items():
-            print(part)
+            print(f"Part '{part_name}': {part}")
             
     def replace_part(self, part_name, new_part):
         if part_name in self.__parts:
             self.__parts[part_name] = new_part
+            
+    def change_part(self, part_name, new_material):
+        if part_name in self.__parts:
+            self.__parts[part_name].change_material(new_material)
+  
+
+class RacingShip(Ship):
+    def __init__(self, ship_name, max_speed):
+        super().__init__(ship_name)
+        self.max_speed = max_speed
+        
+    def display_speed(self):
+        print(f"The maximum speed of {self.ship_name} is {self.max_speed} knots")
+ 
+            
+ship_01 = Ship("Titanic")
+part_001 = Part("Mast", "Wood")
+ship_01._Ship__parts["mast"] = part_001 
+
+print(part_001) 
+ship_01.change_part("mast", "Steel")
+print("\nWe have changed the part's material:")
+print(part_001)  
+
+racing_ship = RacingShip("SpeedBoat", 50)
+print("\nRacing Speed:")
+racing_ship.display_speed()
